1006603851b5c28412b24fe6333b362558969d03
wangym <wang405742530@gmail.com>
2021-09-13 21:30:34 +0800

    提交父子工程,spring security学习项目搭建

---
+++ security-study/src/main/java/com/wong/security/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.wong.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web")
+public class WebController {
+
+    @GetMapping("/home")
+    public String homepage() {
+        return "home";
+    }
+}
