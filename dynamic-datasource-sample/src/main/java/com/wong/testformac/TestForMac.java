efbcec472bf28ae956029df84004578c710e47a0
wym's家用电脑 <wang405742530@163.com>
2023-03-27 22:05:12 +0800

    二次测试分支保护

--- dynamic-datasource-sample/src/main/java/com/wong/testformac/TestForMac.java
+++ dynamic-datasource-sample/src/main/java/com/wong/testformac/TestForMac.java
@@ -21,7 +21,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class TestForMac {
 
     public static void main(String[] args) {
-        System.out.println("测试分支保护");
+        System.out.println("二次测试分支保护");
         SpringApplication.run(TestForMac.class, args);
     }
 
